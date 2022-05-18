bb4af33e7ee4fe48da0678644a29326762cf89bc
Ben David Le <ben@getacademy.no>
2022-05-18 14:57:18 +0200

    Add project files.

---
+++ Objektorientering/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Objektorientering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * Word guess
+             * - Liste av ord som vi velger fra
+             * - Sier ifra hvor mange bokstaver som er like / very cold, cold, hot, very hot
+             */
+
+            List<string> words = new List<string>
+            {
+                "Katt",
+                "Hund",
+                "Papegøye",
+                "Elefant",
+                "Mus"
+            };
+
+            var game = new WordGame(words);
+            var game2 = new WordGame(words);
+            var game3 = new WordGame(words);
+
+            var activeGame = game;
+
+
+            Console.WriteLine("Guess the random word!");
+            while (activeGame.State)
+            {
+                var input = Console.ReadLine().ToLower();
+                activeGame.Guess(input);
+                Console.Clear();
+                activeGame.Show();
+            }
+
+            Console.WriteLine("Press enter to quit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Objektorientering/WordGame.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objektorientering
+{
+
+    internal class WordGame
+    {
+        private readonly Random rnd = new Random();
+
+        //Properties
+        public bool State { get; private set; }
+        public List<string> WordList { get; private set; }
+        private string _randomWord;
+        private double _correctPercent;
+
+
+        //Constructor
+        public WordGame(List<string> wordList)
+        {
+            WordList = wordList;
+            _randomWord = WordList[rnd.Next(0, WordList.Count)].ToLower();
+            State = true;
+        }
+
+        //Metoder
+        public void Show()
+        {
+            var answer = "";
+            switch (_correctPercent)
+            {
+                case < 0.25:
+                    answer = "very cold";
+                    break;
+                case < 0.50:
+                    answer = "cold";
+                    break;
+                case < 0.75:
+                    answer = "hot";
+                    break;
+                case < 0.99:
+                    answer = "very hot";
+                    break;
+                case 1:
+                    answer = "correct";
+                    State = false;
+                    break;
+                default:
+                    answer = "above 1 ?";
+                    break;
+            }
+            Console.WriteLine($"You guess was {answer}!"); //very cold 0 - 25, cold 25 - 50, hot 50 - 75, very hot 75 - 99, correct! 100
+        }
+
+        public void Guess(string guess)
+        {
+            if (guess == _randomWord)
+            {
+                _correctPercent = 1;
+            }
+            else if (guess.Length > _randomWord.Length + 2)
+            {
+                _correctPercent = 0.25;
+            }
+            else
+            {
+                var count = 0D;
+                foreach (var c in guess)
+                {
+                    if (_randomWord.Contains(c))
+                    {
+                        count++;
+                    }
+                }
+
+                _correctPercent = count / _randomWord.Length;
+
+            }
+
+
+        }
+
+    }
+}
